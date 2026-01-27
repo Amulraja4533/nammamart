9467441dc4a9f5f1c5281ba98a3ed62087177ae0
Amulraja4533 <amulraja4533@gmail.com>
2026-01-27 19:56:57 +0530

    Change BASE_IMAGE_URL export to const

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'https://nammamart-backend.onrender.com/api';
 
-export BASE_IMAGE_URL='https://nammamart-backend.onrender.com';
+export const BASE_IMAGE_URL='https://nammamart-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
